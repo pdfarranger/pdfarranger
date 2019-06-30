97b14e591419920b646e75c1da0d649edc94d450
Jerome Robert <jeromerobert@gmx.com>
2019-06-30 14:48:26 +0200

    minor changes in license notices
    * Use hash instead of docstring comments
    * Remove coding: utf-8 (default for python3)
    * Add my name in copyright
    * Add missing license notice in undo.py

--- pdfarranger/pdfarranger.py
+++ pdfarranger/pdfarranger.py
@@ -1,26 +1,18 @@
-# -*- coding: utf-8 -*-
-
-"""
-
- pdfarranger - GTK+ based utility for splitting, rearrangement and
- modification of PDF documents.
- Copyright (C) 2008-2017 Konstantinos Poulios
-
- pdfarranger is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License along
- with this program; if not, write to the Free Software Foundation, Inc.,
- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-"""
+# Copyright (C) 2008-2017 Konstantinos Poulios, 2018-2019 Jerome Robert
+#
+# pdfarranger is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 import os
 import shutil  # for file operations like whole directory deletion
