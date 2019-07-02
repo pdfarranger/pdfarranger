388cef76f69a7111af1ff4db21e8a97f10de5097
Jerome Robert <jeromerobert@gmx.com>
2019-07-02 21:40:35 +0200

    Bump version to 1.3.0

--- pdfarranger/pdfarranger.py
+++ pdfarranger/pdfarranger.py
@@ -63,7 +63,7 @@ else:
         pass
 
 APPNAME = 'PDF Arranger'
-VERSION = '1.2.1'
+VERSION = '1.3.0'
 WEBSITE = 'https://github.com/jeromerobert/pdfarranger'
 LICENSE = 'GNU General Public License (GPL) Version 3.'
 
