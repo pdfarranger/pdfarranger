pdfarranger/pdfarranger

commit fc0749689de57583483c95cfb8441bc17858e889

Reset export file if all pages deleted
When all pages gone, user will probably want to export a brand new PDF
file, so a previously exported file will not be overwritten by an
accidental saving.

pdfarranger/pdfarranger.py
@@ -456,6 +456,7 @@ class PdfArranger(Gtk.Application):
 
         self.model.connect('row-inserted', self.__update_num_pages)
         self.model.connect('row-deleted', self.__update_num_pages)
+        self.model.connect('row-deleted', self.reset_export_file)
 
         # Progress bar
         self.progress_bar = self.uiXML.get_object('progressbar')
@@ -1423,6 +1424,11 @@ class PdfArranger(Gtk.Application):
         about_dialog.connect('response', lambda w, *args: w.destroy())
         about_dialog.connect('delete_event', lambda w, *args: w.destroy())
         about_dialog.show_all()
+
+    def reset_export_file(self, model, _path, _itr=None, _user_data=None):
+        if len(model) == 0:
+            self.set_export_file(None)
+            self.set_unsaved(False)
 
     def __update_num_pages(self, model, _path, _itr=None, _user_data=None):
         self.uiXML.get_object("num_pages").set_text(str(len(model)))

commit 4ae3febdb9dc7d855d5e7fcb91ecf63765a49d19

Fix: 'PdfArranger' object has no attribute 'set_export_file'
* This bug was introduced by fc07496
* Also do not reset the unsaved flag when exporting the current selection

pdfarranger/pdfarranger.py
@@ -533,6 +533,11 @@ class PdfArranger(Gtk.Application):
         self.progress_bar_timeout_id = \
             GObject.timeout_add(50, self.progress_bar_timeout)
 
+    def set_export_file(self, file):
+        if file != self.export_file:
+            self.export_file = file
+            self.set_unsaved(True)
+
     def set_unsaved(self, flag):
         self.is_unsaved = flag
         GObject.idle_add(self.retitle)
@@ -726,10 +731,11 @@ class PdfArranger(Gtk.Application):
             to_export = [row for row in self.model if row.path in selection]
         else:
             self.export_directory = path
-            self.export_file = file_out
-            self.set_unsaved(False)
+            self.set_export_file(file_out)
         m = metadata.merge(self.metadata, self.pdfqueue)
         exporter.export(self.pdfqueue, to_export, file_out, m)
+        if not only_selected:
+            self.set_unsaved(False)
 
     def choose_export_selection_pdf_name(self, _action, _target, _unknown):
         self.choose_export_pdf_name(True)