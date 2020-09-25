ce78e8993f634b20344114e4c14a6d1277e5e70e
Jerome Robert <jeromerobert@gmx.com>
2020-09-25 18:00:37 +0200

    Refactor the croputils.dialog function to 2 class
    Separate the crop frame and the dialog code. So it will be easier to add a new
    frame for the scaling feature.

--- pdfarranger/croputils.py
+++ pdfarranger/croputils.py
@@ -22,87 +22,97 @@ import cairo
 _ = gettext.gettext
 
 
-def dialog(model, selection, window):
-    """Opens a dialog box to define margins for page cropping"""
-
+class _CropWidget(Gtk.Frame):
     sides = ('L', 'R', 'T', 'B')
     side_names = {'L': _('Left'), 'R': _('Right'), 'T': _('Top'), 'B': _('Bottom')}
     opposite_sides = {'L': 'R', 'R': 'L', 'T': 'B', 'B': 'T'}
 
-    def set_crop_value(spinbutton, side):
-        opp_side = opposite_sides[side]
-        adjustment = spin_list[sides.index(opp_side)].get_adjustment()
-        adjustment.set_upper(99.0 - spinbutton.get_value())
-
-    crop = [0.0, 0.0, 0.0, 0.0]
-    if selection:
-        pos = model.get_iter(selection[0])
-        crop = list(model.get_value(pos, 0).crop)
-
-    dialog = Gtk.Dialog(
-        title=(_('Crop Selected Pages')),
-        parent=window,
-        flags=Gtk.DialogFlags.MODAL,
-        buttons=(
-            Gtk.STOCK_CANCEL,
-            Gtk.ResponseType.CANCEL,
-            Gtk.STOCK_OK,
-            Gtk.ResponseType.OK,
-        ),
-    )
-    dialog.set_default_response(Gtk.ResponseType.OK)
-    dialog.set_resizable(False)
-    margin = 12
-    label = Gtk.Label(
-        label=_(
-            'Cropping does not remove any content\n'
-            'from the PDF file, it only hides it.'
+    def __init__(self, model, selection, margin=12):
+        super().__init__(label=_('Crop Margins'))
+        self.props.margin = margin
+        grid = Gtk.Grid()
+        grid.set_column_spacing(margin)
+        grid.set_row_spacing(margin)
+        grid.props.margin = margin
+        self.add(grid)
+        label = Gtk.Label(
+            label=_(
+                'Cropping does not remove any content\n'
+                'from the PDF file, it only hides it.'
+            )
         )
-    )
-    dialog.vbox.pack_start(label, False, False, 0)
-    frame = Gtk.Frame(label=_('Crop Margins'))
-    frame.props.margin = margin
-    dialog.vbox.pack_start(frame, True, True, 0)
-    grid = Gtk.Grid()
-    grid.set_column_spacing(margin)
-    grid.set_row_spacing(margin)
-    grid.props.margin = margin
-    frame.add(grid)
+        label.props.margin = margin
+        grid.attach(label, 0, 0, 3, 1)
+        self.spin_list = []
+        units = 2 * [_('% of width')] + 2 * [_('% of height')]
+        crop = [0.0, 0.0, 0.0, 0.0]
+        if selection:
+            pos = model.get_iter(selection[0])
+            crop = list(model.get_value(pos, 0).crop)
 
-    spin_list = []
-    units = 2 * [_('% of width')] + 2 * [_('% of height')]
-    for row, side in enumerate(sides):
-        label = Gtk.Label(label=side_names[side])
-        label.set_alignment(0, 0)
-        grid.attach(label, 0, row, 1, 1)
+        for row, side in enumerate(_CropWidget.sides):
+            label = Gtk.Label(label=_CropWidget.side_names[side])
+            label.set_alignment(0, 0)
+            grid.attach(label, 0, row + 1, 1, 1)
 
-        adj = Gtk.Adjustment(
-            value=100.0 * crop.pop(0),
-            lower=0.0,
-            upper=99.0,
-            step_increment=1.0,
-            page_increment=5.0,
-            page_size=0.0,
+            adj = Gtk.Adjustment(
+                value=100.0 * crop.pop(0),
+                lower=0.0,
+                upper=99.0,
+                step_increment=1.0,
+                page_increment=5.0,
+                page_size=0.0,
+            )
+            spin = Gtk.SpinButton(adjustment=adj, climb_rate=0, digits=1)
+            spin.set_activates_default(True)
+            spin.connect('value-changed', self.__set_crop_value, self, side)
+            self.spin_list.append(spin)
+            grid.attach(spin, 1, row + 1, 1, 1)
+
+            label = Gtk.Label(label=units.pop(0))
+            label.set_alignment(0, 0)
+            grid.attach(label, 2, row + 1, 1, 1)
+
+    @staticmethod
+    def __set_crop_value(spinbutton, self, side):
+        opp_side = self.opposite_sides[side]
+        adj = self.spin_list[self.sides.index(opp_side)].get_adjustment()
+        adj.set_upper(99.0 - spinbutton.get_value())
+
+    def get_crop(self):
+        return [spin.get_value() / 100.0 for spin in self.spin_list]
+
+
+class Dialog(Gtk.Dialog):
+    """ A dialog box to define margins for page cropping """
+
+    def __init__(self, model, selection, window):
+        super().__init__(
+            title=(_('Crop Selected Pages')),
+            parent=window,
+            flags=Gtk.DialogFlags.MODAL,
+            buttons=(
+                Gtk.STOCK_CANCEL,
+                Gtk.ResponseType.CANCEL,
+                Gtk.STOCK_OK,
+                Gtk.ResponseType.OK,
+            ),
         )
-        spin = Gtk.SpinButton(adjustment=adj, climb_rate=0, digits=1)
-        spin.set_activates_default(True)
-        spin.connect('value-changed', set_crop_value, side)
-        spin_list.append(spin)
-        grid.attach(spin, 1, row, 1, 1)
+        self.set_default_response(Gtk.ResponseType.OK)
+        self.set_resizable(False)
+        self.crop_widget = _CropWidget(model, selection)
+        self.vbox.pack_start(self.crop_widget, False, False, 0)
+        self.show_all()
+        self.selection = selection
 
-        label = Gtk.Label(label=units.pop(0))
-        label.set_alignment(0, 0)
-        grid.attach(label, 2, row, 1, 1)
-
-    dialog.show_all()
-    result = dialog.run()
-
-    crop = None
-    if result == Gtk.ResponseType.OK:
-        crop = [spin.get_value() / 100.0 for spin in spin_list]
-        crop = [crop] * len(selection)
-    dialog.destroy()
-    return crop
+    def run_get(self):
+        """ Open the dialog and return the crop value """
+        result = self.run()
+        crop = None
+        if result == Gtk.ResponseType.OK:
+            crop = [self.crop_widget.get_crop()] * len(self.selection)
+        self.destroy()
+        return crop
 
 
 def white_borders(model, selection, pdfqueue):
