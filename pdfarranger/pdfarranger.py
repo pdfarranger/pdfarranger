dd26b4ca6f55fc6dd01e995ab16f73a75f3d6c0a
kbengs <kbengs@hotmail.com>
2021-04-28 19:49:49 +0300

    Hide iconview when changing window width
    This will make it possible to do smooth width changes even
    if there are a large number of pages in iconview.

--- pdfarranger/pdfarranger.py
+++ pdfarranger/pdfarranger.py
@@ -256,6 +256,8 @@ class PdfArranger(Gtk.Application):
         self.drag_path = None
         self.drag_pos = Gtk.IconViewDropPosition.DROP_RIGHT
         self.sb_timeout_id = None
+        self.window_width_old = 0
+        self.set_iv_visible_id = None
 
         # Clipboard for cut copy paste
         self.clipboard_default = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
@@ -411,6 +413,9 @@ class PdfArranger(Gtk.Application):
         self.window.connect('focus_in_event', self.window_focus_in_out_event)
         self.window.connect('focus_out_event', self.window_focus_in_out_event)
         self.window.connect('configure_event', self.window_configure_event)
+        self.window.connect('button_release_event', self.window_button_release_event)
+        self.window.connect('enter_notify_event', self.window_enter_notify_event)
+        self.window.connect('window_state_event', self.window_state_event)
 
         if hasattr(GLib, "unix_signal_add"):
             GLib.unix_signal_add(GLib.PRIORITY_DEFAULT, signal.SIGINT, self.close_application)
@@ -577,10 +582,42 @@ class PdfArranger(Gtk.Application):
         """Render when vertical scrollbar value has changed."""
         self.silent_render()
 
-    def window_configure_event(self, _window, _event):
-        """Render when window size has changed."""
+    def window_configure_event(self, _window, event):
+        """Handle window size and position changes."""
+        if self.window_width_old not in [0, event.width] and len(self.model) > 0:
+            if self.set_iv_visible_id:
+                GObject.source_remove(self.set_iv_visible_id)
+            self.set_iv_visible_id = GObject.timeout_add(1500, self.set_iconview_visible)
+            self.iconview.set_visible(False)
+        self.window_width_old = event.width
         if len(self.model) > 1: # Don't trigger extra render after first page is inserted
             self.silent_render()
+
+    def window_button_release_event(self, _window, event):
+        """Mouse button release on window."""
+        if event.button == 1:
+            self.set_iconview_visible(timeout=False)
+
+    def window_enter_notify_event(self, _window, _event):
+        """Mouse pointer enter window."""
+        if os.name == 'nt':
+            # In Windows this is triggered when dragging window edge. Instead the release event
+            # is usually triggered when releasing button.
+            return
+        self.set_iconview_visible(timeout=False)
+
+    def window_state_event(self, _window, event):
+        """Window state change."""
+        self.set_iconview_visible(timeout=False)
+
+    def set_iconview_visible(self, timeout=True):
+        if timeout:
+            self.set_iv_visible_id = None
+        if not self.iconview.get_visible():
+            self.update_iconview_geometry()
+            self.scroll_to_selection()
+            GObject.idle_add(self.iconview.set_visible, True)
+            self.iconview.grab_focus()
 
     def set_export_file(self, file):
         if file != self.export_file:
@@ -671,7 +708,8 @@ class PdfArranger(Gtk.Application):
 
     def on_window_size_request(self, _window):
         """Main Window resize."""
-        self.update_iconview_geometry()
+        if self.iconview.get_visible():
+            self.update_iconview_geometry()
 
     def update_iconview_geometry(self):
         """Set iconview cell size, margins, number of columns and spacing."""
@@ -1610,7 +1648,9 @@ class PdfArranger(Gtk.Application):
             self.iv_cursor.cursor_is_visible = False
 
     def window_focus_in_out_event(self, _widget=None, _event=None):
-        """Enable or disable paste actions based on clipboard content."""
+        """Keyboard focus enter or leave window."""
+        self.set_iconview_visible(timeout=False)
+        # Enable or disable paste actions based on clipboard content
         cb_d_data = self.clipboard_default.wait_for_text()
         cb_p_data = os.name == 'posix' and self.clipboard_pdfarranger.wait_for_text()
         data_available = True if cb_d_data or cb_p_data else False
