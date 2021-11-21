pdfarranger/pdfarranger

commit ddee11d7a6e5da02cd6dc93bee0f0320a354b0da

Set minimum pikepdf version to 1.17.0
* All major distro now have it
* Drop a workaround for https://github.com/pdfarranger/pdfarranger/issues/141 /
  https://github.com/pikepdf/pikepdf/issues/80
* Close #460

pdfarranger/exporter.py
@@ -15,10 +15,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
-import copy
 import pikepdf
-import traceback
-import sys
 import os
 import tempfile
 from . import metadata
@@ -149,18 +146,6 @@ def export(input_files, pages, file_out, mode, mdata):
         angle = row.angle
         angle0 = current_page.Rotate if '/Rotate' in current_page else 0
         new_page = pdf_output.copy_foreign(current_page)
-        # Workaround for pikepdf <= 1.10.1
-        # https://github.com/pikepdf/pikepdf/issues/80#issuecomment-590533474
-        try:
-            new_page = copy.copy(new_page)
-        except TypeError:
-            if _report_pikepdf_err:
-                _report_pikepdf_err = False
-                traceback.print_exc()
-                print("Current pikepdf version {}, required pikepdf version "
-                      "1.7.0 or greater. Continuing but PDF Arranger will not "
-                      "work properly.".format(pikepdf.__version__),
-                      file=sys.stderr)
         if angle != 0:
             new_page.Rotate = angle + angle0
         new_page.MediaBox = _mediabox(new_page, row.crop)

setup.py
@@ -51,7 +51,7 @@ setup(
     entry_points={
         'console_scripts': ['pdfarranger=pdfarranger.pdfarranger:main']
     },
-    install_requires=['pikepdf>=1.7.0','python-dateutil>=2.4.0'],
+    install_requires=['pikepdf>=1.17.0','python-dateutil>=2.4.0'],
     extras_require={
         'image': ['img2pdf>=0.3.4'],
     },

commit d1ce9e3b5c928cb93659752cbf9a0adbd0f98bff

Fix geometric transformation on duplicated pages
* This include "split pages" which is implemented as
  duplicate+crop
* This bug was introduced in
  ddee11d7a6e5da02cd6dc93bee0f0320a354b0da
  when dropping workaround for
  https://github.com/pdfarranger/pdfarranger/issues/141
  and https://github.com/pikepdf/pikepdf/issues/80
* Anyway those workaround where not working anymore with
  pikepdf 3 and 4.0.1 because copy.copy was not working
  anymore with pages.
* See also https://github.com/pikepdf/pikepdf/issues/174
* Fix #579

pdfarranger/exporter.py
@@ -103,6 +103,9 @@ def _scale(doc, page, factor):
         Resources={'/XObject': {'/p{}'.format(page_id): xobject}},
         Rotate=rotate,
     )
+    # workaround for pikepdf <= 2.6.0. See https://github.com/pikepdf/pikepdf/issues/174
+    if pikepdf.__version__ < '2.7.0':
+        new_page = doc.make_indirect(new_page)
     return new_page
 
 def check_content(parent, pdf_list):
@@ -153,25 +156,34 @@ def export(input_files, pages, file_out, mode, mdata):
     global _report_pikepdf_err
     pdf_output = pikepdf.Pdf.new()
     pdf_input = [pikepdf.open(p.copyname, password=p.password) for p in input_files]
+    copied_pages = {}
+    # Copy pages from the input PDF files to the output PDF file
     for row in pages:
         current_page = pdf_input[row.nfile - 1].pages[row.npage - 1]
-        new_page = pdf_output.copy_foreign(current_page)
-        _update_angle(row, current_page, new_page)
-        new_page.MediaBox = _mediabox(new_page, row.crop)
-        new_page = _scale(pdf_output, new_page, row.scale)
-
-        # Workraround for pikepdf < 2.7.0
-        # https://github.com/pikepdf/pikepdf/issues/174
-        new_page = pdf_output.make_indirect(new_page)
-
+        # if the page already exists in the output PDF, duplicate it
+        new_page = copied_pages.get((row.nfile, row.npage))
+        if new_page is None:
+            # for backward compatibility with old pikepdf. With pikepdf > 3
+            # new_page = current_page should be enough
+            new_page = pdf_output.copy_foreign(current_page)
+        # let pdf_output adopt new_page
         pdf_output.pages.append(new_page)
+        new_page = pdf_output.pages[-1]
+        copied_pages[(row.nfile, row.npage)] = new_page
         # Ensure annotations are copied rather than referenced
         # https://github.com/pdfarranger/pdfarranger/issues/437
         if pikepdf.Name.Annots in current_page:
             pdf_temp = pikepdf.Pdf.new()
             pdf_temp.pages.append(current_page)
             indirect_annots = pdf_temp.make_indirect(pdf_temp.pages[0].Annots)
-            pdf_output.pages[-1].Annots = pdf_output.copy_foreign(indirect_annots)
+            new_page.Annots = pdf_output.copy_foreign(indirect_annots)
+
+    # Apply geometrical transformations in the output PDF file
+    for page_id, row in enumerate(pages):
+        new_page = pdf_output.pages[page_id]
+        _update_angle(row, new_page, new_page)
+        new_page.MediaBox = _mediabox(new_page, row.crop)
+        pdf_output.pages[page_id] = _scale(pdf_output, new_page, row.scale)
 
     if exportmode in ['ALL_TO_MULTIPLE', 'SELECTED_TO_MULTIPLE']:
         for n, page in enumerate(pdf_output.pages):