pdfarranger/pdfarranger

commit a181d67024aae5beaba8e692d70cfb3165116d69

Fix iconview in right-to-left locale
Close #978

pdfarranger/pdfarranger.py
@@ -1825,7 +1825,10 @@ class PdfArranger(Gtk.Application):
             ref_to = Gtk.TreeRowReference.new(model, self.drag_path)
         else:
             ref_to = None
-        before = self.drag_pos == Gtk.IconViewDropPosition.DROP_LEFT
+        if self.iconview.get_direction() == Gtk.TextDirection.LTR:
+            before = self.drag_pos == Gtk.IconViewDropPosition.DROP_LEFT
+        else:
+            before = self.drag_pos == Gtk.IconViewDropPosition.DROP_RIGHT
         target = selection_data.get_target().name()
         if target == 'MODEL_ROW_INTERN':
             move = context.get_selected_action() & Gdk.DragAction.MOVE
@@ -1932,7 +1935,10 @@ class PdfArranger(Gtk.Application):
             return Gdk.EVENT_STOP
         elif not path or (path == model[-1].path and x_s < x):
             self.drag_path = model[-1].path
-            self.drag_pos = Gtk.IconViewDropPosition.DROP_RIGHT
+            if self.iconview.get_direction() == Gtk.TextDirection.LTR:
+                self.drag_pos = Gtk.IconViewDropPosition.DROP_RIGHT
+            else:
+                self.drag_pos = Gtk.IconViewDropPosition.DROP_LEFT
         else:
             iconview.stop_emission('drag_motion')
             return Gdk.EVENT_PROPAGATE
@@ -2165,16 +2171,11 @@ class PdfArranger(Gtk.Application):
         if target_id == self.TEXT_URI_LIST:
             pageadder = PageAdder(self)
             model = self.iconview.get_model()
-            ref_to = None
-            before = True
-            if len(model) > 0:
-                last_row = model[-1]
-                if self.drag_pos == Gtk.IconViewDropPosition.DROP_LEFT:
-                    ref_to = Gtk.TreeRowReference.new(model, self.drag_path)
-                elif self.drag_path != last_row.path:
-                    iter_next = model.iter_next(model.get_iter(self.drag_path))
-                    path_next = model.get_path(iter_next)
-                    ref_to = Gtk.TreeRowReference.new(model, path_next)
+            ref_to = Gtk.TreeRowReference.new(model, self.drag_path) if len(model) > 0 else None
+            if self.iconview.get_direction() == Gtk.TextDirection.LTR:
+                before = self.drag_pos == Gtk.IconViewDropPosition.DROP_LEFT
+            else:
+                before = self.drag_pos == Gtk.IconViewDropPosition.DROP_RIGHT
             pageadder.move(ref_to, before)
             for uri in selection_data.get_uris():
                 filename = get_file_path_from_uri(uri)

pdfarranger/iconview.py
@@ -171,9 +171,15 @@ class IconviewCursor(object):
             step = 0 if self.cursor_page_nr + columns_nr > len(self.model) - 1 else columns_nr
             self.cursor_page_nr_new = self.cursor_page_nr + step
         elif self.event.keyval == Gdk.KEY_Left:
-            self.cursor_page_nr_new = max(self.cursor_page_nr - 1, 0)
+            if self.iconview.get_direction() == Gtk.TextDirection.LTR:
+                self.cursor_page_nr_new = max(self.cursor_page_nr - 1, 0)
+            else:
+                self.cursor_page_nr_new = min(self.cursor_page_nr + 1, len(self.model) - 1)
         elif self.event.keyval == Gdk.KEY_Right:
-            self.cursor_page_nr_new = min(self.cursor_page_nr + 1, len(self.model) - 1)
+            if self.iconview.get_direction() == Gtk.TextDirection.LTR:
+                self.cursor_page_nr_new = min(self.cursor_page_nr + 1, len(self.model) - 1)
+            else:
+                self.cursor_page_nr_new = max(self.cursor_page_nr - 1, 0)
         elif self.event.keyval == Gdk.KEY_Home:
             self.cursor_page_nr_new = 0
         elif self.event.keyval == Gdk.KEY_End:
@@ -374,23 +380,24 @@ class IconviewDragSelect:
             if path:
                 ind = Gtk.TreePath.get_indices(path)[0]
                 break
+        add = 0.5 if self.iconview.get_direction() == Gtk.TextDirection.LTR else -0.5
         if pos == 'XY':
             location = ind
         elif pos == 'Right':
-            location = ind - 0.5
+            location = ind - add
         elif pos == 'Left':
-            location = ind + 0.5
+            location = ind + add
         elif pos == 'Below':
-            location = ind - self.iconview.get_item_column(path) - 0.5
+            location = ind - self.iconview.get_item_column(path) - add
         elif pos == 'Above':
-            location = ind + self.iconview.get_columns() - self.iconview.get_item_column(path) - 0.5
+            location = ind + self.iconview.get_columns() - self.iconview.get_item_column(path) - add
         elif (y > last_y + last.height) or (y > last_y and x > last_x + last.width):
-            location = len(self.model) - 0.5
+            location = len(self.model) - add
         elif y < 0:
-            location = -0.5
+            location = -add
         else:
             return None
-        return min(location, len(self.model) - 0.5)
+        return min(location, len(self.model) - add)
 
     def set_mouse_cursor(self, cursor_name):
         """Set the cursor type specified by cursor_name."""

commit f7bd89642a0fd89170611d263eb649f8873d4438

Fix reversed page order when dragging in file "after"
Issue introduced in a181d67

pdfarranger/core.py
@@ -714,6 +714,8 @@ class PageAdder:
             if self.pdfqueue_used or len(self.content) > 1 or self.content[0] != 'pdf':
                 self.app.set_unsaved(True)
             self.content = []
+        if not self.before and self.treerowref:
+            self.pages.reverse()
         with self.app.render_lock():
             for p in self.pages:
                 m = [p, p.description()]

pdfarranger/pdfarranger.py
@@ -1419,8 +1419,6 @@ class PdfArranger(Gtk.Application):
         pageadder = PageAdder(self)
         if ref_to:
             pageadder.move(ref_to, before)
-        if not before and ref_to:
-            data.reverse()
 
         for d in data:
             pageadder.addpages(*d)
@@ -1496,7 +1494,7 @@ class PdfArranger(Gtk.Application):
         pastemodes = {0: 'AFTER', 1: 'BEFORE', 2: 'ODD', 3: 'EVEN', 4: 'OVERLAY', 5: 'UNDERLAY'}
         pastemode = pastemodes[mode.get_int32()]
 
-        ref_to, before = self.set_paste_location(pastemode, data_is_filepaths)
+        ref_to, before = self.set_paste_location(pastemode)
 
         if pastemode in ['AFTER', 'BEFORE']:
             if data_is_filepaths:
@@ -1704,7 +1702,7 @@ class PdfArranger(Gtk.Application):
                 d.append((filename, npage, basename, angle, scale, crop, hide, layerdata))
         return d
 
-    def set_paste_location(self, pastemode, data_is_filepaths):
+    def set_paste_location(self, pastemode):
         """Sets reference where pages should be pasted and if before or after that."""
         model = self.iconview.get_model()
 
@@ -1715,17 +1713,10 @@ class PdfArranger(Gtk.Application):
             ref_to = None
         elif pastemode == 'AFTER':
             last_row = model[-1]
+            before = False
             if len(selection) == 0 or selection[-1] == last_row.path:
-                before = False
                 ref_to = None
-            elif data_is_filepaths:
-                before = True
-                path = selection[-1]
-                iter_next = model.iter_next(model.get_iter(path))
-                path_next = model.get_path(iter_next)
-                ref_to = Gtk.TreeRowReference.new(model, path_next)
             else:
-                before = False
                 ref_to = Gtk.TreeRowReference.new(model, selection[-1])
         else:
             if pastemode == 'EVEN':
@@ -1827,7 +1818,7 @@ class PdfArranger(Gtk.Application):
                     imgbufs.append(imgbuf)
         if len(imgbufs) > 0:
             pdf_file_name = _img_to_pdf(imgbufs, self.tmp_dir)
-            ref_to, before = self.set_paste_location(pastemode='AFTER', data_is_filepaths=True)
+            ref_to, before = self.set_paste_location(pastemode='AFTER')
             self.paste_files([pdf_file_name], before, ref_to)
         self.set_export_state(False)
 