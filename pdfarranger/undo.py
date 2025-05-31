5a4ad923ddeed4976547811ba8cd6f423cf27bba
kbengs <kbengs@hotmail.com>
2025-05-31 09:29:53 +0300

    Store vertical adjustment state in undomanager

--- pdfarranger/undo.py
+++ pdfarranger/undo.py
@@ -65,11 +65,13 @@ class Manager(object):
         1. The label of the action
         2. The pages
         3. Which page numbers are selected
+        4. The vertical adjustment percentual value
         """
         pages = [row[0].duplicate(False) for row in self.model]
         s = self.app.iconview.get_selected_items()
         selection = [path.get_indices()[0] for path in s]
-        return (self.label, pages, selection)
+        vadj_percent = self.app.vadj_percent_handler()
+        return (self.label, pages, selection, vadj_percent)
 
     def undo(self, _action, _param, _unused):
         if self.current == len(self.states):
@@ -91,7 +93,7 @@ class Manager(object):
         self.__refresh()
 
     def __set_state(self, state):
-        self.label, pages, selection = state
+        self.label, pages, selection, vadj_percent = state
         self.app.quit_rendering()
         self.app.iconview.unselect_all()
         with self.app.render_lock():
@@ -104,6 +106,7 @@ class Manager(object):
         with GObject.signal_handler_block(self.app.iconview, self.app.id_selection_changed_event):
             for num in selection:
                 self.app.iconview.select_path(self.model[num].path)
+        self.app.vadj_percent = vadj_percent
         self.app.update_iconview_geometry()
         self.app.update_max_zoom_level()
         self.app.retitle()
